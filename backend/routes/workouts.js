f962b879f02a3835a4b53d64fd8af78e3ba040df
andychokim <andychokim@gmail.com>
2025-08-03 19:44:36 -0700

    Lesson 5 - schema and data model

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-
+const workoutModel = require('../models/workoutmodel');
 
 /**
  * routes
@@ -18,8 +18,14 @@ router.get('/:id', (req, res) => {
 });
 
 // POST request to the root path
-router.post('/', (req, res) => {
-    res.json({msg: 'POST a new workout'});
+router.post('/', async (req, res) => {
+    const { title, reps, load } = req.body;
+    try {
+        const workout = await workoutModel.create({title, reps, load});
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
 });
 
 // DELETE request to the root path
